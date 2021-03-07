6b12cbeb6926b53f44e185eb2cf6040217ed95e8
Pajdek <pajdek@gmail.com>
2021-03-07 17:18:18 +0100

    gpiolib: silence invalid GPIO warning

--- drivers/gpio/gpiolib.c
+++ drivers/gpio/gpiolib.c
@@ -108,7 +108,7 @@ struct gpio_desc *gpio_to_desc(unsigned gpio)
 	spin_unlock_irqrestore(&gpio_lock, flags);
 
 	if (!gpio_is_valid(gpio))
-		WARN(1, "invalid GPIO %d\n", gpio);
+		pr_debug("invalid GPIO %d\n", gpio);
 
 	return NULL;
 }
