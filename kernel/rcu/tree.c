f0017a6454ff7006ed19a366a6006369edc5f569
Pajdek <pajdek@gmail.com>
2021-04-19 22:02:24 +0200

    rcu: tree: remove not needed includes

--- kernel/rcu/tree.c
+++ kernel/rcu/tree.c
@@ -52,8 +52,6 @@
 #include <linux/smpboot.h>
 #include <linux/jiffies.h>
 #include <linux/slab.h>
-#include <linux/sched/isolation.h>
-#include <linux/sched/clock.h>
 #include "../time/tick-internal.h"
 
 #include "tree.h"
